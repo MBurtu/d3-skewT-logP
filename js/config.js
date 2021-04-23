e82ceda459bfcd7f5342473a94c717ecc468d4cf
Mattias Burtu <79286138+MBurtu@users.noreply.github.com>
2021-04-23 10:55:02 +0200

    Adds a copyright section

--- js/config.js
+++ js/config.js
@@ -18,6 +18,7 @@
 //          4.2 Domain & Axis
 //      5. International Standard Atmosphere
 //      6. Various path generators
+//      7. Copyrights
 
 //-
 //-
@@ -171,3 +172,28 @@ var hodoline = d3.lineRadial()
     
 // Bisector function for tooltips    
 var bisectTemp = d3.bisector(function(d) { return d.pres; }).left;
+
+/////////////////////////////////
+//
+//      7. Copyrights
+//
+/////////////////////////////////
+
+/*
+The Skew-T diagram and hodograph are based on https://github.com/rsobash/d3-skewt
+
+MIT License
+
+Copyright (c) 2016 Ryan Sobash
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+*/
