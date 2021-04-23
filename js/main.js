e1a5f2fa0ec027291da4a5d362f8676de3015d62
Mattias Burtu <79286138+MBurtu@users.noreply.github.com>
2021-04-23 10:50:11 +0200

    Adds a copyright section

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@
 //          4.1 Load data
 //          4.2 Draw data
 //      5. Parcel profiles
+//      6. Copyrights
 
 //-
 //-
@@ -1943,3 +1944,28 @@ function testBound(pxls,type) {
     return pxls;
 
 }
+
+/////////////////////////////////
+//
+//      6. Copyrights
+//
+/////////////////////////////////
+
+/*
+The Skew-T diagram and hodograph are based on https://github.com/rsobash/d3-skewt
+
+MIT License
+
+Copyright (c) 2016 Ryan Sobash
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+*/
