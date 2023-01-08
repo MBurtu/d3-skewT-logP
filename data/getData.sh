b9e585e97a7da3cde5b37c4d7485f2e501001833
Mattias Burtu <mattias.burtu@gmail.com>
2023-01-08 16:17:05 +0100

    First commit

---
+++ data/getData.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+SECONDS=0
+
+########################
+#
+# User input
+#
+########################
+
+TODAY=`date +%Y%m%d`
+MODEL_RUN=$1  # 00 06 12 18
+START_HR=0
+END_HR=72
+STEP=3
+
+TODAYS_MODEL=$TODAY$MODEL_RUN #YYYYMMDDHH
+
+###################################
+echo 'Downloading GFS data...'
+###################################
+
+for i in {0..72..3}
+do 
+    if  [ $i -lt 10 ]
+    then
+        i="00${i}"   
+    fi
+    if  [ $i -ge 10 ]
+    then
+        i="0${i}"   
+    fi
+    # grib filter
+    # https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?dir=%2Fgfs.20210222%2F06
+    URL="https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${MODEL_RUN}z.pgrb2.0p25.f${i}&lev_1000_mb=on&lev_100_mb=on&lev_10_m_above_ground=on&lev_150_mb=on&lev_200_mb=on&lev_250_mb=on&lev_2_m_above_ground=on&lev_300_mb=on&lev_350_mb=on&lev_400_mb=on&lev_450_mb=on&lev_500_mb=on&lev_550_mb=on&lev_600_mb=on&lev_650_mb=on&lev_700_mb=on&lev_750_mb=on&lev_800_mb=on&lev_850_mb=on&lev_900_mb=on&lev_925_mb=on&lev_950_mb=on&lev_975_mb=on&lev_surface=on&var_HGT=on&var_PRES=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&subregion=&leftlon=-5.0&rightlon=32.5&toplat=71.5&bottomlat=48.0&dir=%2Fgfs.${TODAY}%2F${MODEL_RUN}%2Fatmos"
+    
+    curl "$URL" -o "gfs.t${MODEL_RUN}z.pgrb2.0p25.f${i}"
+
+done
+#perl get_gfs.pl data $TODAYS_MODEL $START_HR $END_HR $STEP all "PRES|HGT|TMP|SPFH|UGRD|VGRD" .
+
+###################################
+echo 'Converting grib to json...'
+###################################
+python3 grib2json.py $TODAY $MODEL_RUN $END_HR $STEP
+
+echo ''
+###################################
+echo 'Removing grib data..'
+###################################
+rm gfs.t*
+
+echo ''
+if (( $SECONDS > 60 )) ; then
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Completed in $minutes minute(s) and $seconds second(s)"
+else
+    echo "Completed in $SECONDS seconds"
+fi
---
+++ data/grib2json.py
@@ -0,0 +1,215 @@
+####################################################
+#
+#    Converting grib to json
+#  
+####################################################
+
+import sys
+from datetime import datetime, timedelta
+import math
+import pygrib
+import json
+import metpy.calc as calc
+from metpy.units import units
+
+####################################################
+
+# user input
+input = sys.argv
+yyyy = int(input[1][:4])
+mm = int(input[1][4:6])
+dd = int(input[1][6:8])
+model_run = input[2]
+end_hr = int(input[3])
+step = int(input[4])
+
+locations = {'Arlanda': {'lat':59.75, 'lon':18.0},
+             'Landvetter': {'lat':57.75, 'lon':12.25},
+             'Ostersund': {'lat':63.25, 'lon':14.5},
+             'Gallivare': {'lat':67.25, 'lon':20.75},
+             'Orsa': {'lat':61.0, 'lon':14.75},
+             'Umea': {'lat':63.75, 'lon':20.25},
+             'Vaxjo': {'lat':57.0, 'lon':14.75},
+             'Arvidsjaur': {'lat':65.5, 'lon':19.25},
+             'Visby': {'lat':57.75, 'lon':18.5},
+             'Orebro': {'lat':59.25, 'lon':15.0},
+             'Karlstad': {'lat':59.50, 'lon':13.25},
+             'Kastrup': {'lat':55.75, 'lon':12.0},
+             'Karup': {'lat':56.25, 'lon':9.0},
+             'Gardemoen': {'lat':60.25, 'lon':11.0},
+             'Helsinki': {'lat':60.25, 'lon':25.0},
+             'Turku': {'lat':60.5, 'lon':22.25},
+             'Vaasa': {'lat':63.0, 'lon':21.75},
+             'Jyvaskyla': {'lat':62.25, 'lon':25.75},
+             'Rovaniemi': {'lat':66.5, 'lon':25.75},
+             'Joensuu': {'lat':62.75, 'lon':29.50},
+             'Tallinn': {'lat':59.25, 'lon':25.0},
+             'Riga': {'lat':57.0, 'lon':24.0},
+             'Vilnius': {'lat':54.75, 'lon':25.25},
+             'Hamburg': {'lat':53.75, 'lon':10.0},
+             'Berlin': {'lat':52.25, 'lon':13.5},
+             'Gdansk': {'lat':54.5, 'lon':18.25},
+             'Warszawa': {'lat':52.25, 'lon':21.0},
+             'StPetersburg': {'lat':59.75, 'lon':30.25},
+             'Heathrow': {'lat':51.5, 'lon':-0.5},
+             'CharlesDeGaulle': {'lat':49.0, 'lon':2.5},
+             'Schiphol': {'lat':52.25, 'lon':4.75},
+             'Prague': {'lat':50.0, 'lon':14.25},
+             'BothnianSea': {'lat':61.75, 'lon':19.5},
+             'NorthSea': {'lat':56.25, 'lon':4.25},
+             'NorwegianSea': {'lat':65.75, 'lon':6.25}
+            }
+
+sounding = {}
+date = datetime(yyyy,mm,dd,int(model_run))
+forecast = range(0,end_hr+step,step)
+
+for timestep in forecast: 
+
+    if timestep < 10:
+        timestep = f'0{timestep}'
+
+    print('')
+    print(f'Timestep: +{timestep}') 
+
+    grbs = pygrib.open(f'gfs.t{model_run}z.pgrb2.0p25.f0{timestep}')
+
+    Psfc = grbs.select(name='Surface pressure')
+    O = grbs.select(name='Orography')
+    T = grbs.select(name='Temperature')
+    T2m = grbs.select(name='2 metre temperature')
+    GH = grbs.select(name='Geopotential Height')
+    RH = grbs.select(name='Relative humidity')
+    RH2m = grbs.select(name='2 metre relative humidity')
+    U = grbs.select(name='U component of wind')
+    U10m = grbs.select(name='10 metre U wind component')
+    V = grbs.select(name='V component of wind')
+    V10m = grbs.select(name='10 metre V wind component')
+
+    levels = [100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,925,950,975,1000] #hPa
+
+    for name, loc in locations.items():
+
+        print(name)
+
+        location = {}
+
+        latitude = loc["lat"]
+        longitude = loc["lon"]
+      
+        location[f'{date}'] = []
+
+        temperatures = []
+        dewpoints = []
+        heights_agl = []
+        speeds = []
+        directions = []
+        pres_lvls = []
+        
+        #############################
+        #
+        #   Surface
+        #
+        #############################
+
+        sfc_height, lat, lon = O[0].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+        sfc_pressure, lat, lon = Psfc[0].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+        temperature_2m, lat, lon = T2m[0].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+        relative_humidity_2m, lat, lon = RH2m[0].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+        u_wind_10m, lat, lon = U10m[0].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+        v_wind_10m, lat, lon = V10m[0].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+    
+        sfc_pressure = round(float(sfc_pressure)/100) #Pa -> hPa
+        sfc_height = float(sfc_height)
+
+        temperature_2m = round(float(temperature_2m) - 273.15,1) #Kelvin -> Celsius
+        relative_humidity_2m = float(relative_humidity_2m)
+        dewpoint_temperature_2m = calc.dewpoint_from_relative_humidity((temperature_2m*units.degC).to(units.K),relative_humidity_2m/100).to(units('degC'))
+        dewpoint_temperature_2m = round(float(dewpoint_temperature_2m.magnitude),1)
+
+        wind_speed_10m = math.sqrt(float(u_wind_10m)**2 + float(v_wind_10m)**2)#*1.944 #m/s -> kt
+
+        math_dir_10m = math.degrees(math.atan2(float(v_wind_10m),float(u_wind_10m)))
+        wind_dir_10m = 270 - math_dir_10m
+        if wind_dir_10m > 360: wind_dir_10m -= 360
+
+        #############################
+        #
+        #   Atmosphere
+        #
+        #############################
+
+        for i in range(0,len(levels)):
+            if levels[i] < (sfc_pressure-25):
+
+                pres_lvls.append(levels[i])
+
+                temperature, lat, lon = T[i].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+                geopotential_height, lat, lon = GH[i].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+                relative_humidity, lat, lon = RH[i].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+                u_wind, lat, lon = U[i].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+                v_wind, lat, lon = V[i].data(lat1=latitude,lat2=latitude,lon1=longitude,lon2=longitude)
+
+                temperature = round(float(temperature) - 273.15,1) #Kelvin -> Celsius
+                temperatures.append(temperature)
+
+                relative_humidtity = float(relative_humidity)
+                if relative_humidity == 0:
+                    relative_humidity = 0.0000001
+                dewpoint_temperature = calc.dewpoint_from_relative_humidity((temperature*units.degC).to(units.K),relative_humidity/100).to(units('degC'))
+                dewpoint_temperature = round(float(dewpoint_temperature.magnitude),1)
+                dewpoints.append(dewpoint_temperature)
+
+                wind_speed = math.sqrt(float(u_wind)**2 + float(v_wind)**2)
+                speeds.append(round(wind_speed,1))
+
+                math_dir = math.degrees(math.atan2(float(v_wind),float(u_wind)))
+                wind_dir = 270 - math_dir
+                if wind_dir > 360: wind_dir -= 360
+                directions.append(round(wind_dir))
+
+                height = float(geopotential_height)
+                
+                hghtagl = height - sfc_height
+                heights_agl.append(round(hghtagl))
+
+        # Add surface values
+        pres_lvls.append(sfc_pressure)
+        heights_agl.append(2)
+        temperatures.append(temperature_2m)
+        dewpoints.append(dewpoint_temperature_2m)
+        directions.append(round(wind_dir_10m))
+        speeds.append(round(wind_speed_10m,1))        
+        
+        sounding[f'{name}-{timestep}'] = {
+            'pres': pres_lvls[::-1],
+            'hghtagl': heights_agl[::-1],
+            'tmpc': temperatures[::-1],
+            'dwpc': dewpoints[::-1],
+            'wdir': directions[::-1],
+            'wspd': speeds[::-1]
+            }
+            
+    # Update date
+    date = date + timedelta(hours=step)
+
+print('')
+print('Write to file...')    
+for name, loc in locations.items():
+    
+    date = datetime(yyyy,mm,dd,int(model_run))
+    new_sounding = {}
+
+    for timestep in forecast: 
+
+        if timestep < 10:
+            timestep = f'0{timestep}'
+
+        new_sounding[f'{date}'] = sounding[f'{name}-{timestep}']
+        
+        date = date + timedelta(hours=step)
+    
+    print(f'sounding_{name}.json')
+
+    with open(f'sounding_{name}.json', 'w') as outfile:
+        json.dump(new_sounding, outfile)
